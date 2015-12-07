4c64d140f454ac293ecaa384d16f3e666e56a6f0
theuglysweater <jmarks@IT103.mr.preble.com>
2015-12-07 15:21:43 -0500

    UI project files

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    [SerializeField]
+    Color beggining;
+
+    [SerializeField]
+    Color end;
+
+    [SerializeField]
+    Image healthBarFill;
+
+    [SerializeField]
+    Image healthBarSlowFill;
+
+    [SerializeField]
+    uint MaxHealth;
+
+    [SerializeField]
+    uint CurrentHealth;
+
+    [SerializeField]
+    uint damageAmount;
+
+    [SerializeField]
+    float slowSpeed;
+
+    [SerializeField]
+    float freezeTimer;
+
+    float Percent { get { return (float)CurrentHealth / MaxHealth; } }
+
+    //float slowPercent { get { return (float)CurrentHealthSlow / MaxHealth; } }
+
+    void Start()
+    {
+        healthBarFill.color = beggining;
+        CurrentHealth = MaxHealth;
+        healthBarSlowFill.fillAmount = healthBarFill.fillAmount;
+    }
+
+    void Update()
+    {
+        if (freezeTimer > 0)
+        {
+            freezeTimer -= Time.deltaTime;
+        }
+
+        if(CurrentHealth > MaxHealth)
+        {
+            CurrentHealth = MaxHealth;
+        }
+
+        healthBarFill.color = Color.Lerp(end, beggining, Percent);
+        healthBarFill.fillAmount = Percent;
+
+        if (freezeTimer <= 0)
+        {
+            healthBarSlowFill.fillAmount += (healthBarFill.fillAmount - healthBarSlowFill.fillAmount) / slowSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            takeDamage();
+        }
+    }
+
+    void takeDamage()
+    {
+        CurrentHealth -= damageAmount;
+        freezeTimer = 0.5f;
+    }
+}
---
+++ Assets/Scripts/SuperMeter.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SuperMeter : MonoBehaviour {
+
+    [SerializeField]
+    Image superFill;
+
+    [SerializeField]
+    Image superBack;
+
+    [SerializeField]
+    float fillAmount;
+
+    [SerializeField]
+    byte powerLevel;
+
+    [SerializeField]
+    Color[] levelColors;
+
+    [SerializeField]
+    float fillSpeed = 1;
+
+    public int Levels { get { return levelColors.Length - 1; } }
+
+    void Start()
+    {
+        Debug.Log(fillAmount);
+        ChangeColors();
+        superFill.fillAmount = 0;
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            fillAmount += 0.1f;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            fillAmount += 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            fillAmount += 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            
+            if(!UsePower(1))Debug.Log("IT'S NO USE!");
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+
+            if (!UsePower(3)) Debug.Log("IT'S NO USE!");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            if (!UsePower(5)) Debug.Log("IT'S NO USE!");
+        }
+        
+        if(powerLevel == Levels)
+        {
+            fillAmount = 0;
+        }
+
+        if (fillAmount >= 1)
+        {
+            fillAmount--;
+            powerLevel = (byte)Mathf.Clamp(powerLevel + 1, 0, Levels);
+            superFill.fillAmount = fillAmount;
+            ChangeColors();
+        }
+
+        superFill.fillAmount += (fillAmount - superFill.fillAmount) / fillSpeed;
+    }
+
+    void ChangeColors()
+    {
+        superBack.color = levelColors[powerLevel];
+        superFill.color = levelColors[powerLevel + 1];
+    }
+
+    bool UsePower(byte amount)
+    {
+        if (amount > powerLevel) return false;
+            
+        else
+        {
+            powerLevel -= amount;
+            ChangeColors();
+            return true;
+        }
+    }
+
+
+
+
+
+}
