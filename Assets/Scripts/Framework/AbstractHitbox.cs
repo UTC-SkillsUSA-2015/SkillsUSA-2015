36c9a16b40a758e403e296ffb831fb6a9170b0b6
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-02-02 12:02:57 -0500

    Begun work on Hitboxes

---
+++ Assets/Scripts/Framework/AbstractHitbox.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractHitbox : MonoBehaviour {
+    [SerializeField]
+    Collider2D hitbox;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    /// <summary>
+    /// Override this so the derived classes know how to behave when contacted.
+    /// </summary>
+    /// <param name="collision">The collider on the hitbox being contacted.</param>
+    public abstract void OnTriggerEnter2D (Collider2D collision);
+    /// <summary>
+    /// Called for attacks that launch the opponent. Blocking or evading may nullify this.
+    /// </summary>
+    /// <param name="launchForce">The direction and magnitude of the launch.</param>
+    public abstract void Launch (Vector2 launchForce);
+    /// <summary>
+    /// Damages the opponent. Blocking or evading may nullify this.
+    /// </summary>
+    /// <param name="dmg">Damage to take.</param>
+    public abstract void Damage (float dmg);
+    /// <summary>
+    /// Damages the opponent, but still does a small percent of damage if
+    /// blocked.
+    /// </summary>
+    /// <param name="dmg">The amount of damage to deal.</param>
+    /// <param name="chipPercent">The percent of damage dealt if blocked.</param>
+    public abstract void Damage (float dmg, float chipPercent);
+}
---
+++ Assets/Scripts/UniversalHitbox.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UniversalHitbox : AbstractHitbox {
+    public enum HitboxState {
+        Normal,
+        Block,
+        Attack,
+        Dodge
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public override void OnTriggerEnter2D (Collider2D collision) {
+        throw new NotImplementedException ();
+    }
+
+    public override void Launch (Vector2 launchForce) {
+        throw new NotImplementedException ();
+    }
+
+    public override void Damage (float dmg) {
+        throw new NotImplementedException ();
+    }
+
+    public override void Damage (float dmg, float chipPercent) {
+        throw new NotImplementedException ();
+    }
+}
