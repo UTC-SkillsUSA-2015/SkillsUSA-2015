d6c74b7be02248a28eea6e48e0e3e787fad4627a
theuglysweater <jmarks@IT403_REDONE.mr.preble.com>
2016-02-29 12:55:40 -0500

    WORK DAMMIT

--- Assets/Scripts/TestHitbox.cs
+++ Assets/Scripts/TestHitbox.cs
@@ -4,7 +4,8 @@ using System;
 
 public class TestHitbox : AbstractHitbox {
     public override void Hit (Attack attack) {
-		Debug.Log ("I've been for " + attack.kData.Dmg + " by " + attack.kData.name + "!!", gameObject);
+		Debug.Log ("I've been hit by " + attack.kData.name + "!!", gameObject);
+		Debug.Log ("BLAM");
     }
 
     public override void OnTriggerEnter2D (Collider2D collision) {
