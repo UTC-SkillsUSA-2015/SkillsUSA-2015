9eacb3dd08638eb9de9e536a3590df2c6e648bf5
theuglysweater <jmarks@mr.preble.com>
2016-03-08 12:34:39 -0500

    JointAnimator SCript

--- Assets/Scripts/JointAnimator.cs
+++ Assets/Scripts/JointAnimator.cs
@@ -4,83 +4,104 @@ using System.Collections;
 [ExecuteInEditMode]
 public class JointAnimator : MonoBehaviour {
 
-    [Range(0, 135)]
-    public float LeftLowerArmANGLE;
-    [SerializeField]
-    GameObject LeftLowerArm;
+    public Joints mabones;
 
     [Range(0, 135)]
-    public float RightLowerArmANGLE;
-    [SerializeField]
-    GameObject RightLowerArm;
+    public float LeftLowerArmANGLE;    
+
+    [Range(0, 135)]
+    public float RightLowerArmANGLE;    
 
     [Range(-90, 90)]
-    public float LeftShoulderANGLE;
-    [SerializeField]
-    GameObject LeftShoulder;
+    public float LeftShoulderANGLE;    
 
     [Range(-90, 90)]
-    public float RightShoulderANGLE;
-    [SerializeField]
-    GameObject RightShoulder;
+    public float RightShoulderANGLE;   
 
     [Range(-45, 10)]
     public float LeftHandANGLE;
-    [SerializeField]
-    GameObject LeftHand;
 
     [Range(-45, 10)]
-    public float RightHandANGLE;
-    [SerializeField]
-    GameObject RightHand;
+    public float RightHandANGLE;   
 
     [Range(-30, 70)]
-    public float UpperTorsoANGLE;
-    [SerializeField]
-    GameObject UpperTorso;
+    public float UpperTorsoANGLE;    
 
     [Range(-10, 45)]
-    public float WaistANGLE;
-    [SerializeField]
-    GameObject Waist;
+    public float WaistANGLE;   
 
     [Range(-30, 80)]
     public float LeftThighANGLE;
-    [SerializeField]
-    GameObject LeftThigh;
 
     [Range(-30, 80)]
-    public float RightThighANGLE;
-    [SerializeField]
-    GameObject RightThigh;
+    public float RightThighANGLE;   
 
     [Range(-135, 0)]
-    public float LeftLowerLegANGLE;
-    [SerializeField]
-    GameObject LeftLowerLeg;
+    public float LeftLowerLegANGLE;    
 
     [Range(-135, 0)]
-    public float RightLowerLegANGLE;
-    [SerializeField]
-    GameObject RightLowerLeg;
+    public float RightLowerLegANGLE;    
 
     [Range(-70, 0)]
-    public float LeftFootANGLE;
-    [SerializeField]
-    GameObject LeftFoot;
+    public float LeftFootANGLE;    
 
     [Range(-70, 0)]
-    public float RightFootANGLE;
-    [SerializeField]
-    GameObject RightFoot;
+    public float RightFootANGLE;    
 
     [Range(-60, 60)]
-    public float HeadANGLE;
-    [SerializeField]
-    GameObject Head;
+    public float HeadANGLE;   
 
     void Update()
     {
-
+        mabones.LeftLowerArm.transform.localRotation = Quaternion.Euler(0, 0, LeftLowerArmANGLE);
+        mabones.RightLowerArm.transform.localRotation = Quaternion.Euler(0, 0, RightLowerArmANGLE);
+        mabones.LeftShoulder.transform.localRotation = Quaternion.Euler(0, 0, LeftShoulderANGLE);
+        mabones.RightShoulder.transform.localRotation = Quaternion.Euler(0, 0, RightShoulderANGLE);
+        mabones.LeftHand.transform.localRotation = Quaternion.Euler(0, 0, LeftHandANGLE);
+        mabones.RightHand.transform.localRotation = Quaternion.Euler(0, 0, RightHandANGLE);
+        mabones.UpperTorso.transform.localRotation = Quaternion.Euler(0, 0, UpperTorsoANGLE);
+        mabones.Waist.transform.localRotation = Quaternion.Euler(0, 0, WaistANGLE);
+        mabones.LeftThigh.transform.localRotation = Quaternion.Euler(0, 0, LeftThighANGLE);
+        mabones.RightThigh.transform.localRotation = Quaternion.Euler(0, 0, RightThighANGLE);
+        mabones.LeftLowerLeg.transform.localRotation = Quaternion.Euler(0, 0, LeftLowerLegANGLE);
+        mabones.RightLowerLeg.transform.localRotation = Quaternion.Euler(0, 0, RightLowerLegANGLE);
+        mabones.LeftFoot.transform.localRotation = Quaternion.Euler(0, 0, LeftFootANGLE);
+        mabones.RightFoot.transform.localRotation = Quaternion.Euler(0, 0, RightFootANGLE);
+        mabones.Head.transform.localRotation = Quaternion.Euler(0, 0, HeadANGLE);
     }
+}
+
+[System.Serializable]
+public class Joints
+{
+    public GameObject LeftLowerArm;
+
+    public GameObject RightLowerArm;
+
+    public GameObject LeftShoulder;
+
+    public GameObject RightShoulder;
+
+    public GameObject LeftHand;
+
+    public GameObject RightHand;
+
+    public GameObject UpperTorso;
+
+    public GameObject Waist;
+
+    public GameObject LeftThigh;
+
+    public GameObject RightThigh;
+
+    public GameObject LeftLowerLeg;
+
+    public GameObject RightLowerLeg;
+
+    public GameObject LeftFoot;
+
+    public GameObject RightFoot;
+
+    public GameObject Head;
+
 }
