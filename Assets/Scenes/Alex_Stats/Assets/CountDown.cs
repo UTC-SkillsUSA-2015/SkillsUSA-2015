a867457dd587ef6b7b1dfb1dd5410cac8c1022df
Alex Rediker <redikeralex@utc4me.org>
2016-03-18 11:58:17 -0400

    Countdown Scene
    Mechanically appealing yes, Visually appealing... Not so much

---
+++ Assets/Scenes/Alex_Stats/Assets/CountDown.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDown : MonoBehaviour 
+{
+	public GameObject one;
+	public GameObject two;
+	public GameObject three;
+	public GameObject fight;
+	public GameObject block;
+
+	void Start () 
+	{
+		StopCoroutine (Countdwn ());
+		StartCoroutine (Countdwn ());
+	}
+	
+	IEnumerator Countdwn () 
+	{
+		yield return new WaitForSeconds (0.5f);
+		three.SetActive (true);
+		yield return new WaitForSeconds (0.5f);
+		three.SetActive (false);
+		yield return new WaitForSeconds (0.5f);
+		two.SetActive (true);
+		yield return new WaitForSeconds (0.5f);
+		two.SetActive (false);
+		yield return new WaitForSeconds (0.5f);
+		one.SetActive (true);
+		yield return new WaitForSeconds (0.5f);
+		one.SetActive (false);
+		yield return new WaitForSeconds (0.5f);
+		fight.SetActive (true);
+		yield return new WaitForSeconds (1f);
+		fight.SetActive (false);
+		block.SetActive (false);
+	}
+}
