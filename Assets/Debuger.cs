e2fc0a340987a25b0652510e9231aa58b85cc171
theuglysweater <jmarks@mr.preble.com>
2016-04-01 13:05:00 -0400

    I DONE DID IT

---
+++ Assets/Debuger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debuger : MonoBehaviour {
+
+	void Update()
+    {
+        string[] names = Input.GetJoystickNames();
+        Debug.Log(names[0]);
+        Debug.Log(names[1]);
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            Debug.Log("Hello from a");
+        }
+    }
+}
