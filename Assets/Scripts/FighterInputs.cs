afbd7954745f83b3ec4ef5f8c2b074844b94164b
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-04-04 12:56:52 -0400

    FighterInputs and Fighter Update
    wow I need to make sure I'm on the right branch more often

--- Assets/Scripts/FighterInputs.cs
+++ Assets/Scripts/FighterInputs.cs
@@ -3,14 +3,55 @@ using System.Collections;
 
 [CreateAssetMenu(fileName = "Fighter Controls", menuName = "Fighter Controls")]
 public class FighterInputs : ScriptableObject {
-    [SerializeField]
-    public string HorizontalAxis;
-    [SerializeField]
-    public string Jump;
-    [SerializeField]
-    public string LightAttack;
-    [SerializeField]
-    public string MediumAttack;
-    [SerializeField]
-    public string HeavyAttack;
+    public string[] HorizontalAxis;
+    public string[] VerticalAxis;
+    public string[] LightAttack;
+    public string[] MediumAttack;
+    public string[] HeavyAttack;
+
+    public float HorizontalInput {
+        get {
+            return firstAxis (HorizontalAxis);
+        }
+    }
+    public float VerticalInput {
+        get {
+            return firstAxis (VerticalAxis);
+        }
+    }
+    public bool LightInput {
+        get {
+            return firstButton (LightAttack);
+        }
+    }
+    public bool MediumInput {
+        get {
+            return firstButton (MediumAttack);
+        }
+    }
+    public bool HeavyInput {
+        get {
+            return firstButton (HeavyAttack);
+        }
+    }
+
+    private float firstAxis(string[] axes) {
+        var i = 0f;
+        foreach (var axis in axes) {
+            i = Input.GetAxisRaw (axis);
+            if (i != 0)
+                break;
+        }
+        return i;
+    }
+
+    private bool firstButton (string[] buttons) {
+        var i = false;
+        foreach (var button in buttons) {
+            i = Input.GetButtonDown (button);
+            if (i)
+                break;
+        }
+        return i;
+    }
 }
