6609cac7eb4871d50b6fc33dc20c8d26491b0f11
Alex Rediker <redikeralex@utc4me.org>
2016-06-21 15:50:17 -0400

    Merge remote-tracking branch 'refs/remotes/origin/master' into Menu_Branch2.0
    # Conflicts:
    #	Assets/Animations/TyreseControll.controller
    #	Assets/Prefabs/Characters/SmileyBasic.prefab
    #	Assets/Prefabs/Characters/TyreseBasic.prefab
    #	ProjectSettings/EditorBuildSettings.asset
    #	ProjectSettings/InputManager.asset

---
+++ Assets/ScreenCap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+using UnityStandardAssets;
+
+public class ScreenCap : MonoBehaviour {
+
+	public GameObject flash;
+
+	void Start()
+	{
+		flash.SetActive(false);
+		GetComponent<GrayScale> ().enable = false;
+	}
+	void Capture ()
+	{
+		if(Input.GetKey(KeyCode.Space))
+		{
+			Snap();
+		}
+	}
+
+	IEnumerator Snap()
+	{
+		flash.SetActive (true);
+		Time.timeScale = 0;
+
+		flash.SetActive(false);
+		yield return new WaitForSeconds (3);
+		Time.timeScale = 1;
+	}
+}
