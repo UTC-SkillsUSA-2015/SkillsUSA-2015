dfc7dfdc95833dcc30dec91edf75aefaa45e35c0
theuglysweater <jmarks@utc4me.org>
2016-06-22 15:49:00 -0400

    clear

--- Assets/Scripts/Framework/ICommandParser.cs
+++
@@ -1,3 +0,0 @@
-﻿public interface ICommandParser {
-    void ParseString (string command);
-}
--- Assets/Scripts/Framework/IControlEngine2D.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-
-public interface IControlEngine2D {
-    /// <summary>
-    /// Motion the engine will take.
-    /// </summary>
-    Vector2 Motion { set; }
-}
